e6aa8e0337d7bc0544a823e3617008fb02023db2
arkhanpathan <armankhanjnv@gmail.com>
2023-07-17 21:27:49 +0530

    updated turbo stream notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,22 +2,7 @@ class Notification < ApplicationRecord
   belongs_to :user
   scope :unviewed, -> { where(viewed: false ) }
   default_scope { latest }
-  
-  after_create_commit do 
-    broadcast_prepend_to "broadcast_to_user_#{self.user_id}", 
-      target: :notifications
-  end
-  
-  after_update_commit do 
-    broadcast_replace_to "broadcast_to_user_#{self.user_id}", 
-      target: self
-  end
-  
-  after_destroy_commit do 
-    broadcast_remove_to "broadcast_to_user_#{self.user_id}", 
-      target: :notifications
-  end
-  
+
   after_commit do
     broadcast_replace_to "broadcast_to_user_#{self.user_id}", 
       target: "notifications_count", 
